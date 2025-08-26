6a9cd2fa91622bde5d40c141e7b297848cb73a34
Lonmethgkhoi <cantrang83@gmail.com>
2025-08-27 01:24:47 +0700

    config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+function getConfig() {
+    return {
+        timer: 0,
+        redirect: true,
+        redirectv2: false,
+        redirectv2Delay: 0,
+        lootlabsv2: true,
+        redirectURLButton: ['krnl.cat', 'ads.luarmor.net'],
+        cooldownButton: 'default',
+        auto_copy: true,
+        pause_on_captcha: true
+    };
+}
